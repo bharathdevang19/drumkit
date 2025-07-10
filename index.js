333796aa5e50e1fe3211a81ca548aa9848dd7c12
Bharath N J <159224757+bharathdevang19@users.noreply.github.com>
2025-07-10 23:52:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,31 +11,31 @@ document.addEventListener("keydown", function (event) {
 function makesound(key) {
     switch (key) {
         case "w":
-            var audio1 = new Audio("/Drum+Kit+Starting+Files/Drum Kit Starting Files/sounds/crash.mp3");
+            var audio1 = new Audio("/sounds/crash.mp3");
             audio1.play();
             break;
         case "a":
-            var audio2 = new Audio("/Drum+Kit+Starting+Files/Drum Kit Starting Files/sounds/kick-bass.mp3");
+            var audio2 = new Audio("/sounds/kick-bass.mp3");
             audio2.play();
             break;
         case "s":
-            var audio3 = new Audio("/Drum+Kit+Starting+Files/Drum Kit Starting Files/sounds/snare.mp3");
+            var audio3 = new Audio("/sounds/snare.mp3");
             audio3.play();
             break;
         case "d":
-            var audio4 = new Audio("/Drum+Kit+Starting+Files/Drum Kit Starting Files/sounds/tom-1.mp3");
+            var audio4 = new Audio("/sounds/tom-1.mp3");
             audio4.play();
             break;
         case "j":
-            var audio5 = new Audio("/Drum+Kit+Starting+Files/Drum Kit Starting Files/sounds/tom-2.mp3");
+            var audio5 = new Audio("/sounds/tom-2.mp3");
             audio5.play();
             break;
         case "k":
-            var audio6 = new Audio("/Drum+Kit+Starting+Files/Drum Kit Starting Files/sounds/tom-3.mp3");
+            var audio6 = new Audio("/sounds/tom-3.mp3");
             audio6.play();
             break;
         case "l":
-            var audio7 = new Audio("/Drum+Kit+Starting+Files/Drum Kit Starting Files/sounds/tom-4.mp3");
+            var audio7 = new Audio("/sounds/tom-4.mp3");
             audio7.play();
             break;
 
